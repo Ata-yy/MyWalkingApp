d9bd16515ce17831755e0ef47f68efd320bd3999
Ata-yy <93063275+Ata-yy@users.noreply.github.com>
2021-12-25 16:56:00 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,52 @@
+'use strict';
+
+{
+  // Intersection Observer API
+  // IntersectionObserver
+  function inViewcallback(entries, obs) {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+        return;
+      }
+
+      entry.target.classList.add('appear');
+      obs.unobserve(entry.target);
+    });
+  }
+
+  function onScrollcallback(entries) {
+    entries.forEach(entry => {
+      if(!entry.isIntersecting) {
+        header.classList.add('scrolled');
+        toTop.classList.add('scrolled');
+      } else {
+        header.classList.remove('scrolled');
+        toTop.classList.remove('scrolled');
+      }
+    });
+  }
+
+  const inViewobserver = new IntersectionObserver(inViewcallback, {threshold: 0.2});
+
+  document.querySelectorAll('.animate').forEach(el => {
+    inViewobserver.observe(el);
+  });
+
+
+
+  // onScrollobserver
+  const onScrollobserver = new IntersectionObserver(onScrollcallback);
+  onScrollobserver.observe(document.getElementById('target'));
+  const header = document.querySelector('header');
+  const toTop = document.getElementById('to_top');
+
+  // click
+  toTop.addEventListener('click', e => {
+    e.preventDefault();
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
+
+}
